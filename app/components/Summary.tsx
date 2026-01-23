4473690acf29af795fb151605bc7214e92b6d9b0
AceProgrms <151991514+AceProgrms@users.noreply.github.com>
2026-01-23 08:16:32 +0100

    continued working on summary comp

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ScoreGauge from "~/components/ScoreGauge";
 
+const Category = ({title, score} : {title: string, score: number}) => {
+    return(
+
+    )
+}
+
 const Summary = ({feedback}:  {feedback : Feedback}) => {
     return (
         <div className='bg-white rounded -2xl shadow-md w-full'>
             <div className="flex flex-row items-center p-4 gap-8">
                 <ScoreGauge score={feedback.overallScore}/>
+                <div className="text-2xl font-bold">Your Resume Score</div>
+                <p className="text-sm text-gray-500">
+                    This score is calculated based on the variables listed below.
+                </p>
             </div>
         </div>
     );
